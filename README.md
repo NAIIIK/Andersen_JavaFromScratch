c179a02e8e6eb06a2bfc8522969c65d81694c860
RodionovNikita <radzivonaumikita@gmail.com>
2024-05-29 16:21:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java From Scratch
+
+1. Screenshot is located in **src/main/resources**
---
+++ src/main/java/com/andersenlab/lecture_1/Main.java
@@ -0,0 +1,8 @@
+package com.andersenlab.lecture_1;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Nikita";
+        System.out.printf("Hello %s\n", name);
+    }
+}
