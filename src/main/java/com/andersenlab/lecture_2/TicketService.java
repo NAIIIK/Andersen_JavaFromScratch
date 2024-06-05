d620199271f8180e31b1aa9e43abd4601f181845
RodionovNikita <radzivonaumikita@gmail.com>
2024-06-05 22:48:48 +0200

    JFB-1: edit TicketService class
    Edited TicketService class to manage tickets

--- src/main/java/com/andersenlab/lecture_2/TicketService.java
+++ src/main/java/com/andersenlab/lecture_2/TicketService.java
@@ -1,16 +1,14 @@
 package com.andersenlab.lecture_2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TicketService {
 
-    public static void main(String[] args) {
-        long time = System.currentTimeMillis() / 1000L;
+    List<Ticket> tickets;
 
-        Ticket emptyTicket = new Ticket();
-        Ticket limitedTicket = new Ticket("Hall 1", 303, time);
-        Ticket fullTicket = new Ticket(234, "Hall 2", 399, time, true, 'C', 5.355, 99.99);
-
-        System.out.println("Ticket with no args constructor:\n" + emptyTicket);
-        System.out.println("Ticket with limited args constructor:\n" + limitedTicket);
-        System.out.println("Ticket with all args constructor:\n" + fullTicket);
+    public TicketService() {
+        tickets = new ArrayList<>();
     }
+
 }
