02d5665abc4af6ea87ba41aad8533301e0612209
RodionovNikita <radzivonaumikita@gmail.com>
2024-06-05 22:57:55 +0200

    JFB-1: edit TicketService class
    Added methods to create and add tickets with the help of TicketService class

--- src/main/java/com/andersenlab/lecture_2/TicketService.java
+++ src/main/java/com/andersenlab/lecture_2/TicketService.java
@@ -11,4 +11,26 @@ public class TicketService {
         tickets = new ArrayList<>();
     }
 
+    public void createEmptyTicket() {
+        capacityChecker();
+        Ticket emptyTicket = new Ticket();
+        tickets.add(emptyTicket);
+    }
+
+    public void createLimitedTicket(String concertHall, int eventCode, long time) {
+        capacityChecker();
+        Ticket limitedTicket = new Ticket(concertHall, eventCode, time);
+        tickets.add(limitedTicket);
+    }
+
+    public void createFullTicket(String concertHall, int eventCode, long time, boolean isPromo,
+                                 char stadiumSector, double maxAllowedWeight, double price) {
+        capacityChecker();
+        Ticket fullTicket = new Ticket(concertHall, eventCode, time, isPromo, stadiumSector, maxAllowedWeight, price);
+        tickets.add(fullTicket);
+    }
+
+    private void capacityChecker() {
+        if (tickets.size() >= 10) throw new RuntimeException("Ticket limit reached");
+    }
 }
