b0b7f79b586625cafba9dc9b5fb5adb03d7547c4
Dana <Dana.ackerman0914@gmail.com>
2024-06-07 20:38:44 +0500

    Tested in main function

---
+++ src/main/java/com/andersenlab/lecture_2/Main.java
@@ -0,0 +1,21 @@
+package com.andersenlab.lecture_2;
+
+
+public class Main {
+    public static void main(String[] args) {
+        TicketService service = new TicketService();
+        long unixTime = System.currentTimeMillis() / 1000L;
+
+        service.createTicket();
+        service.createTicket("Hall 1", 151, unixTime);
+        service.createTicket("Hall 2", 155, unixTime, true,
+                'C', 5.555, 99.99);
+
+
+        System.out.println(service.getById(2));
+        System.out.println(service.getStorageInfo());
+
+        System.out.println(service.getStadiumSector('C'));
+
+    }
+}
