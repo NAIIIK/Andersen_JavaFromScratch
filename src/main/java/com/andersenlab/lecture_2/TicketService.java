5bad1de050d89dc936c630e06ddf561eb7eb6d48
Dana <Dana.ackerman0914@gmail.com>
2024-06-07 21:21:51 +0500

    some changes

--- src/main/java/com/andersenlab/lecture_2/TicketService.java
+++ src/main/java/com/andersenlab/lecture_2/TicketService.java
@@ -9,7 +9,9 @@ public class TicketService {
         Ticket limitedTicket = new Ticket("Hall 1", 303, time);
         Ticket fullTicket = new Ticket(234, "Hall 2", 399, time, true, 'C', 5.355, 99.99);
 
-        System.out.println("Ticket with no args constructor:\n" + emptyTicket);
+//        System.out.println("Ticket with no args constructor:\n" + emptyTicket);
+
+
         System.out.println("Ticket with limited args constructor:\n" + limitedTicket);
         System.out.println("Ticket with all args constructor:\n" + fullTicket);
     }
