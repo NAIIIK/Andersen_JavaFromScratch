40b904c8e3f8ad28ff0e6f3da2dc70f88333fc79
RodionovNikita <radzivonaumikita@gmail.com>
2024-06-07 18:19:57 +0200

    Testing merge conflict

--- src/main/java/com/andersenlab/lecture_2/TicketService.java
+++ src/main/java/com/andersenlab/lecture_2/TicketService.java
@@ -9,7 +9,7 @@ public class TicketService {
         Ticket limitedTicket = new Ticket("Hall 1", 303, time);
         Ticket fullTicket = new Ticket(234, "Hall 2", 399, time, true, 'C', 5.355, 99.99);
 
-        System.out.println("Ticket with no args constructor:\n" + emptyTicket);
+        System.out.println("Empty ticket:\n" + emptyTicket);
         System.out.println("Ticket with limited args constructor:\n" + limitedTicket);
         System.out.println("Ticket with all args constructor:\n" + fullTicket);
     }
