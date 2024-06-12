924aaacabbf13cf529e3ece2ba86bad7bb24a91a
RodionovNikita <radzivonaumikita@gmail.com>
2024-06-12 18:58:47 +0200

    JFB-4: create Entity class
    created new abstract class Entity with "ID" field and possibility to set and get "ID"

---
+++ src/main/java/com/andersenlab/lecture_2/Entity.java
@@ -0,0 +1,13 @@
+package com.andersenlab.lecture_2;
+
+public abstract class Entity {
+    public int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
