d0382018454a64d54bee09fe6a6ab0714556e784
RodionovNikita <radzivonaumikita@gmail.com>
2024-06-12 20:47:35 +0200

    JFB-4: create NullableWarningChecker class

---
+++ src/main/java/com/andersenlab/lecture_2/NullableWarningChecker.java
@@ -0,0 +1,21 @@
+package com.andersenlab.lecture_2;
+
+import java.lang.reflect.Field;
+
+public class NullableWarningChecker {
+    public static void checkNulls(Object o) {
+        Field[] fields = o.getClass().getDeclaredFields();
+        for (Field field: fields) {
+            if (field.isAnnotationPresent(NullableWarning.class)) {
+                try {
+                    field.setAccessible(true);
+                    if (field.get(o) == null) {
+                        System.out.println("Variable " + field.getName() + " is null");
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
