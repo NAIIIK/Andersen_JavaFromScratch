5c163f6890747664e303114e955fe7b3e15b0b75
RodionovNikita <radzivonaumikita@gmail.com>
2024-06-12 20:56:59 +0200

    JFB-4: edit Main class
    added the example of the chosen polymorphism approach for Client and Admin classes

--- src/main/java/com/andersenlab/lecture_2/Main.java
+++ src/main/java/com/andersenlab/lecture_2/Main.java
@@ -8,10 +8,24 @@ public class Main {
 
         service.createTicket();
         service.createTicket("Hall 1", 151, unixTime);
-        service.createTicket("Hall 2", 155, unixTime, true,
+        service.createTicket(1234, "Hall 2", 155, unixTime, true,
                 'C', 5.555, 99.99);
 
         System.out.println(service.getById(2));
         System.out.println(service.getStorageInfo());
+
+        User client1 = new Client(1223);
+        User admin1 = new Admin(3221);
+        client1.printRole();
+        admin1.printRole();
+
+        Client client2 = new Client(1332);
+        Admin admin2 = new Admin(3112);
+
+        client2.printRole();
+        client2.getTicket();
+
+        admin2.printRole();
+        admin2.checkTicket();
     }
 }
