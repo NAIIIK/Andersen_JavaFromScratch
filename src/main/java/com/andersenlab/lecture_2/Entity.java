d84ab79f10f388d3ad9e05e4610e6dcbcbbfc9ad
RodionovNikita <radzivonaumikita@gmail.com>
2024-06-12 19:04:08 +0200

    JFB-4: edit Entity class
    relocated idChecker() method from Ticket class

--- src/main/java/com/andersenlab/lecture_2/Entity.java
+++ src/main/java/com/andersenlab/lecture_2/Entity.java
@@ -8,6 +8,12 @@ public abstract class Entity {
     }
 
     public void setId(int id) {
+        idChecker(id);
+
         this.id = id;
+    }
+
+    private void idChecker(int id) {
+        if (id / 1000 > 9) throw new TicketArgumentException("ID must contain no more than 4 digits");
     }
 }
